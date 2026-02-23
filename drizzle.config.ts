3f5e92507eb61574760ca99e4ce5c8f1a8c6702c
Miena Basta Badres <dev@mbadres.com>
2026-02-23 10:50:41 +0100

    feat: update database configuration to use environment variables and add Bulma dependency

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } =
+	process.env;
+if (!POSTGRES_HOST || !POSTGRES_PORT || !POSTGRES_USER || !POSTGRES_PASSWORD || !POSTGRES_DB)
+	throw new Error('One or more POSTGRES_* env variables are not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'postgresql',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	dbCredentials: {
+		url: `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`,
+	},
 	verbose: true,
 	strict: true
 });
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,17 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (
+	!env.POSTGRES_HOST ||
+	!env.POSTGRES_PORT ||
+	!env.POSTGRES_USER ||
+	!env.POSTGRES_PASSWORD ||
+	!env.POSTGRES_DB
+)
+	throw new Error('One or more POSTGRES_* env variables are not set');
 
-const client = postgres(env.DATABASE_URL);
+const client = postgres(
+	`postgres://${env.POSTGRES_USER}:${env.POSTGRES_PASSWORD}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`
+);
 
 export const db = drizzle(client, { schema });
